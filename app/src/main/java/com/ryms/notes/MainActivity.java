4d20bdd28c584799d8da95dbd52d2944757bb6b3
Sahajpal <sahajpal80@gmail.com>
2021-05-06 12:18:39 +0530

    Implemented View Model

--- app/src/main/java/com/ryms/notes/MainActivity.java
+++ app/src/main/java/com/ryms/notes/MainActivity.java
@@ -1,14 +1,29 @@
 package com.ryms.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(MainActivity.this, "onchanged", Toast.LENGTH_SHORT).show();;
+            }
+        });
     }
 }
